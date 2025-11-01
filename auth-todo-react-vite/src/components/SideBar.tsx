6681138c0a2a99e4a04c924eb65e9abcb5926155
odewale emmanuel <odebzemmygee@gmail.com>
2025-11-02 00:09:45 +0100

    feat: kinde auth

--- auth-todo-react-vite/src/components/SideBar.tsx
+++ auth-todo-react-vite/src/components/SideBar.tsx
@@ -1,14 +1,35 @@
 import { AddTodoForm } from "./AddTodoForm";
 import { Button } from "./Button";
 
+import { useKindeAuth } from "@kinde-oss/kinde-auth-react";
+
 export function SideBar() {
+  const { login, logout, register, isAuthenticated } = useKindeAuth();
+
+  function handleLogin() {
+    login();
+  }
+
+  function handleLogout() {
+    logout();
+  }
+
   return (
     <section className="flex flex-col justify-between col-[2/3] row-[2/3] bg-[#fffcf9] border-l border-black/[0.08] px-[25px] py-[18px]">
       <AddTodoForm />
 
       <div className="flex flex-col gap-2">
-        <Button variant="secondary">Login</Button>
-        <Button variant="secondary">Register</Button>
+        <Button
+          variant="secondary"
+          onClick={isAuthenticated ? handleLogout : handleLogin}
+        >
+          {isAuthenticated ? "Logout" : "Login"}
+        </Button>
+        {!isAuthenticated && (
+          <Button variant="secondary" onClick={() => register()}>
+            Register
+          </Button>
+        )}
       </div>
     </section>
   );
--- auth-todo-react-vite/src/main.tsx
+++ auth-todo-react-vite/src/main.tsx
@@ -3,11 +3,19 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { TodoContextProvider } from "./context/TodoContextProvider.tsx";
+import { KindeProvider } from "@kinde-oss/kinde-auth-react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <TodoContextProvider>
-      <App />
-    </TodoContextProvider>
+    <KindeProvider
+      clientId="20b3e7f34c9249708c4baa48d5b55651"
+      domain="https://authtodo.kinde.com"
+      redirectUri="http://localhost:5173"
+      logoutUri="http://localhost:5173"
+    >
+      <TodoContextProvider>
+        <App />
+      </TodoContextProvider>
+    </KindeProvider>
   </StrictMode>
 );
